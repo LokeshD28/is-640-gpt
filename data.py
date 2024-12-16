06985a78f623df5becdb8aa6e24971cc002600d4
Lokesh D <72072531+LokeshD28@users.noreply.github.com>
2024-12-16 12:37:16 -0800

    Update data.py

--- data.py
+++ data.py
@@ -1,22 +1,19 @@
-import torch
-
+import torch 
+#Processes text into tensor with encoding.
 class Data:
     def __init__(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             self.text = f.read()
-        self.chars = sorted(list(set(self.text)))
-        self.vocab_size = len(self.chars)
-        self.stoi = {ch: i for i, ch in enumerate(self.chars)} 
-        self.itos = {i: ch for i, ch in enumerate(self.chars)} 
-
+        each_char = sorted(list(set(self.text)))
+        self.vocab_size = len(each_char)
+        self.stoi = {ch: i for i, ch in enumerate(each_char)} 
+        self.itos = {i: ch for i, ch in enumerate(each_char)}  
     def encode(self, s):
-        """Converts a string to a list of integers."""
         return [self.stoi[c] for c in s]
 
     def decode(self, l):
-        """Converts a list of integers back to a string."""
         return ''.join([self.itos[i] for i in l])
 
     def get_data(self):
-        """Returns the encoded data."""
         return torch.tensor(self.encode(self.text), dtype=torch.long)
+    
