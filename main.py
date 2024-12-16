a2c0601d8b1e6a97cb591a3d615b60044aa74fa6
Lokesh D <72072531+LokeshD28@users.noreply.github.com>
2024-12-16 12:35:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ import torch
 from trainer import Trainer
 from data import Data
 from model import GPTLanguageModel
+ 
 
-RANDOM_SEED = 1337
-TRAIN_ITERATIONS = 10000
-WORD_COUNT = 100
-DATA_FILE = "input.txt"
+class static_variables() :
+
+
+    BATCH_SIZE = 16 
+    BLOCK_SIZE = 32 
+    MAX_ITERS = 10000
+    EVAL_INTERVAL = 1000
+    LEARNING_RATE = 1E-3
+    EVAL_ITERS = 200
+    RANDOM_SEED = 1337
+    WORD_COUNT = 10000
+    n_embd =  64
+    n_head =  4
+    n_layer =  4
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    dropout = 0.2
+
+
+#this is main part 
 
 def main():
-    torch.manual_seed(RANDOM_SEED)
 
+    DATA_FILE = "input.txt"
+    torch.manual_seed(static_variables.RANDOM_SEED)
     data = Data(DATA_FILE)
-    model = GPTLanguageModel(data.vocab_size)
-
-    trainer = Trainer(data, model)
-    trainer.train(TRAIN_ITERATIONS)
-
-    context = torch.zeros((1, 1), dtype=torch.long)
-    generated = model.generate(context, WORD_COUNT)[0].tolist()
-
+    model = GPTLanguageModel(data.vocab_size,static_variables)
+    trainer = Trainer(data.get_data(), model,static_variables)
+    context =trainer.train(static_variables.MAX_ITERS)
+    # print(context)
+    generated = model.generate(context, static_variables.WORD_COUNT)[0].tolist()
     print(data.decode(generated))
-
 
 if __name__ == "__main__":
     main()
