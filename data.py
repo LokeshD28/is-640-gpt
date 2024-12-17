2cebe61b7426b91d289a8e150e2a8be72b6c26cc
dhrutidedhia17 <dhrutidedhia17@gmail.com>
2024-12-16 20:13:19 -0800

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import torch
 #Processes text into tensor with encoding.
 class Data:
     def __init__(self, file_path):
-        with open(file_path, 'r', encoding='utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8') as f
             self.text = f.read()
         each_char = sorted(list(set(self.text)))
         self.vocab_size = len(each_char)
